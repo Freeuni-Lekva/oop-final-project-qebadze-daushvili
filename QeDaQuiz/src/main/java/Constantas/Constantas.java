ffcb955d5888de5ed5a7b6c33b1c74d38bc2a821
lizikutchava <lkuch23@freeuni.edu.ge>
2025-06-22 00:48:26 +0400

    Account done

---
+++ QeDaQuiz/src/main/java/Constantas/Constantas.java
@@ -0,0 +1,9 @@
+public class Constantas {
+
+    public static final int AMATEUR_AUTHOR_QUIZES_MADE = 1;
+    public static final int PROLIFIC_AUTHOR_QUIZES_MADE = 5;
+    public static final int PRODIGIOUS_AUTHOR_QUIZES_MADE = 10;
+
+    public static final int QUIZ_MACHINE_QUIZES_TAKEN = 10;
+
+}
